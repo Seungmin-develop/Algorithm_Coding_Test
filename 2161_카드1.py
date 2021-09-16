1c534efdff85527c42c37292a9d7f8b2fb406cdb
tmdals8798@gmail.com <tmdals9887@naver.com>
2021-09-16 15:46:17 +0900

    #2161

--- 2161_카드1.py
+++ 2161_카드1.py
@@ -6,7 +6,16 @@ input = sys.stdin.readline
 
 N = int(input())
 queue = deque()
+count = 0
+temp = 0
 for i in range(1,N+1):
     queue.append(i)
+    count+=1
 
-print(queue)
+while count>1:
+    print(queue.popleft(), end=' ')
+    count-=1
+    temp = queue.popleft()
+    queue.append(temp)
+
+print(queue[0])
