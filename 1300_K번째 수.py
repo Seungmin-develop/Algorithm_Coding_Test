b8c466c4d8e2186e05772c2f949e73a5ce5ed873
Seungmin-develop <tmdals8798@gmail.com>
2021-10-27 00:18:00 +0900

    #9184

--- 1300_K번째 수.py
+++ 1300_K번째 수.py
@@ -4,9 +4,21 @@ from bisect import bisect_left, bisect_right
 sys.setrecursionlimit(10000)
 input = sys.stdin.readline
 
-N = int(input())
-queue = deque()
-for i in range(1,N+1):
-    queue.append(i)
+N, K = int(input()), int(input())
 
-print(queue)
+start = 1
+end = K # B[K]는 절대 K번째 수보다 클 수 없다.
+res = 0
+
+while start<=end:
+    mid = (start+end)//2
+    count = 0
+    for i in range(1,N+1):
+        count+=min(mid//i, N)
+    if count>=K:
+        end = mid-1
+        res = mid
+    else:
+        start = mid+1
+
+print(res)
--- 2110_공유기 설치.py
+++ 2110_공유기 설치.py
@@ -4,10 +4,33 @@ from bisect import bisect_left, bisect_right
 sys.setrecursionlimit(10000)
 input = sys.stdin.readline
 
-N = int(input())
-A = [[0]*N]*N
-for i in range(N):
-    for j in range(N):
-        A[i][j] = (i+1)*(j+1)
+def binary_search(start, end):
+    while start<=end:
+        count = 1
+        mid = (start+end)//2
+        current = houses[0]
+        for house in houses:
+            if house >= current+mid:
+                count+=1
+                current = house
+        if count>=C:
+            start = mid+1
+            global answer
+            answer = mid
+        else:
+            end = mid-1
 
-print(A)
+
+N, C = map(int, input().split())
+houses = []
+answer = 0
+
+for _ in range(N):
+    houses.append(int(input()))
+
+houses.sort()
+start = 1
+end = houses[-1]-houses[0]
+
+binary_search(start, end)
+print(answer)
