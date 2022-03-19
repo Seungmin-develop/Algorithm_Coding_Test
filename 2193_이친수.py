5343a99e03dd31e9a194d3942052aa90a0d58ae6
Seungmin-develop <tmdals8798@gmail.com>
2022-03-19 13:11:45 +0900

    #2193

---
+++ 2193_이친수.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+dp = [[0]*(2) for _ in range(N+1)]
+
+dp[1][1] = 1
+
+for i in range(2, N+1):
+    dp[i][1] = dp[i-1][0]
+    dp[i][0] = dp[i-1][0] + dp[i-1][1]
+
+print(dp[N][0]+dp[N][1])
